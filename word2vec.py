3c885ac6db8335cabd2377ab8b73d87153f7f457
Jeroen Vuurens <j.b.p.vuurens@tudelft.nl>
2015-06-22 12:23:21 +0200

    word2vec

---
+++ word2vec.py
@@ -0,0 +1,120 @@
+# example: https://www.kaggle.com/c/word2vec-nlp-tutorial/details/part-2-word-vectors
+# Download the data files below (need to signup)
+# unlabeledTrainData.tsv : https://www.kaggle.com/c/word2vec-nlp-tutorial/download/unlabeledTrainData.tsv.zip
+# labeledTrainData.tsv : https://www.kaggle.com/c/word2vec-nlp-tutorial/download/labeledTrainData.tsv.zip
+# testData.tsv : https://www.kaggle.com/c/word2vec-nlp-tutorial/download/testData.tsv.zip
+
+import pandas as pd
+from gensim.models import word2vec
+
+# Read data from files
+train = pd.read_csv( "labeledTrainData.tsv", header=0, delimiter="\t", quoting=3 )
+test = pd.read_csv( "testData.tsv", header=0, delimiter="\t", quoting=3 )
+unlabeled_train = pd.read_csv( "unlabeledTrainData.tsv", header=0, delimiter="\t", quoting=3 )
+
+# Verify the number of reviews that were read (100,000 in total)
+print "Read %d labeled train reviews, %d labeled test reviews, " \
+ "and %d unlabeled reviews\n" % (train["review"].size, test["review"].size, unlabeled_train["review"].size )
+
+# Import various modules for string cleaning
+from bs4 import BeautifulSoup
+import re
+from nltk.corpus import stopwords
+
+def review_to_wordlist( review, remove_stopwords=False ):
+    # Function to convert a document to a sequence of words,
+    # optionally removing stop words.  Returns a list of words.
+    #
+    # 1. Remove HTML
+    review_text = BeautifulSoup(review).get_text()
+    #
+    # 2. Remove non-letters
+    review_text = re.sub("[^a-zA-Z]"," ", review_text)
+    #
+    # 3. Convert words to lower case and split them
+    words = review_text.lower().split()
+    #
+    # 4. Optionally remove stop words (false by default)
+    if remove_stopwords:
+        stops = set(stopwords.words("english"))
+        words = [w for w in words if not w in stops]
+    #
+    # 5. Return a list of words
+    return(words)
+
+# Download the punkt tokenizer for sentence splitting
+import nltk.data
+# uncomment the downloader to open a download window that allows you to
+# download the punkt tokenizers
+
+# nltk.download()
+
+# Load the punkt tokenizer
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+# Define a function to split a review into parsed sentences
+def review_to_sentences( review, tokenizer, remove_stopwords=False ):
+    # Function to split a review into parsed sentences. Returns a
+    # list of sentences, where each sentence is a list of words
+    #
+    # 1. Use the NLTK tokenizer to split the paragraph into sentences
+    raw_sentences = tokenizer.tokenize(review.strip())
+    #
+    # 2. Loop over each sentence
+    sentences = []
+    for raw_sentence in raw_sentences:
+        # If a sentence is empty, skip it
+        if len(raw_sentence) > 0:
+            # Otherwise, call review_to_wordlist to get a list of words
+            sentences.append( review_to_wordlist( raw_sentence, \
+              remove_stopwords ))
+    #
+    # Return the list of sentences (each sentence is a list of words,
+    # so this returns a list of lists
+    return sentences
+
+sentences = []  # Initialize an empty list of sentences
+
+print "Parsing sentences from training set"
+for review in train["review"]:
+    sentences += review_to_sentences(review.decode("utf8"), tokenizer)
+
+
+print "Parsing sentences from unlabeled set"
+for review in unlabeled_train["review"]:
+    sentences += review_to_sentences(review.decode("utf8"), tokenizer)
+
+
+# print sentences[0]
+# [u'with', u'all', u'this', u'stuff', u'going', u'down', u'at', u'the', u'moment', u'with', u'mj', u'i', u've', u'started', u'listening', u'to', u'his', u'music', u'watching', u'the', u'odd', u'documentary', u'here', u'and', u'there', u'watched', u'the', u'wiz', u'and', u'watched', u'moonwalker', u'again']
+# print sentences[1]
+# [u'maybe', u'i', u'just', u'want', u'to', u'get', u'a', u'certain', u'insight', u'into', u'this', u'guy', u'who', u'i', u'thought', u'was', u'really', u'cool', u'in', u'the', u'eighties', u'just', u'to', u'maybe', u'make', u'up', u'my', u'mind', u'whether', u'he', u'is', u'guilty', u'or', u'innocent']
+
+# Import the built-in logging module and configure it so that Word2Vec
+# creates nice output messages
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',\
+    level=logging.INFO)
+
+# Set values for various parameters
+num_features = 300    # Word vector dimensionality
+min_word_count = 40   # Minimum word count
+num_workers = 4       # Number of threads to run in parallel
+context = 10          # Context window size
+downsampling = 1e-3   # Downsample setting for frequent words
+
+# Initialize and train the model (this will take some time)
+from gensim.models import word2vec
+print "Training model..."
+model = word2vec.Word2Vec(sentences, workers=num_workers, \
+            size=num_features, min_count = min_word_count, \
+            window = context, sample = downsampling)
+
+# If you don't plan to train the model any further, calling
+# init_sims will make the model much more memory-efficient.
+model.init_sims(replace=True)
+
+# It can be helpful to create a meaningful model name and
+# save the model for later use. You can load it later using Word2Vec.load()
+model_name = "300features_40minwords_10context"
+model.save(model_name)
